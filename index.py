f63af0a597c580a7ab40a0ec1b7636e66cb5fb9b
Nurlan3007 <maratnurlan3007@gmail.com>
2025-01-07 21:44:36 +0500

    thir_commit

--- index.py
+++ index.py
@@ -2,28 +2,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 print("Hello Akbota")
-# Загрузка данных из файла
-file_path = "datasets/abalone.data"  # Укажите путь к вашему файлу
+
+file_path = "datasets/abalone.data"
 columns = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight',
            'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings']
 
-# Загрузка данных в DataFrame
+
 df = pd.read_csv(file_path, header=None, names=columns)
 
-# Просмотр первых строк
+
 print(df.head())
 
-# Scatter Plot: Length vs Rings
-plt.figure(figsize=(8, 6))
-sns.scatterplot(data=df, x='Length', y='Rings', hue='Sex', palette='Set2', s=100)
-plt.title('Length vs Rings (Age)')
-plt.xlabel('Length (mm)')
-plt.ylabel('Rings (Age without 1.5)')
-plt.legend(title='Sex')
-plt.grid(True)
-plt.show()
+def show_scat_plot_with_name_Rings(column_name,ff='mm'):
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=df, x=column_name, y='Rings', hue='Sex', palette='Set2', s=100)
+    plt.title(str(column_name) + ' vs Rings (Age)')
+    plt.xlabel(str(column_name) + ', ' + ff)
+    plt.ylabel('Rings (Age without 1.5)')
+    plt.legend(title='Sex')
+    plt.grid(True)
+    plt.show()
 
-# Корреляционная матрица
+show_scat_plot_with_name_Rings("Length")
+show_scat_plot_with_name_Rings("Height")
+show_scat_plot_with_name_Rings("Diameter")
+show_scat_plot_with_name_Rings("Whole weight",'gg')
+show_scat_plot_with_name_Rings("Viscera weight",'gg')
+show_scat_plot_with_name_Rings("Shell weight",'gg')
+
+
+
+
+
+
+
 corr_matrix = df[['Length', 'Diameter', 'Height', 'Whole weight',
                   'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings']].corr()
 
